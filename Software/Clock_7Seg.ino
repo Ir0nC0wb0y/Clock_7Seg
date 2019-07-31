5fb44b2bad890b867bf78eb153e00158d731735a
Ir0nC0wb0y <51864419+Ir0nC0wb0y@users.noreply.github.com>
2019-07-30 23:34:15 -0500

    added Wifi and NTP time keeping

--- Software/Clock_7Seg.ino
+++ Software/Clock_7Seg.ino
@@ -1,6 +1,14 @@
 #define FASTLED_INTERRUPT_RETRY_COUNT 0
 #define FASTLED_ESP8266_RAW_PIN_ORDER
 #include <FastLED.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUDP.h>
+#include <NTPClient.h>
+
+// Necessary libraries for WiFiManager
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
 
 // FastLED Variables
 #define NUM_LEDS_HR      14
@@ -10,13 +18,21 @@
 #define CLOCK_BRIGHTNESS  5
 
 // Time Animation
-int grad_hr = 0;
-int grad_mn = 120;
-int grad_dir_hr = 1;
-int grad_dir_mn = 1;
-int time_hour = 0;
+#define HOUR_FORMAT      12
+int grad_hr     =   0;
+int grad_mn     = 120;
+int grad_dir_hr =   1;
+int grad_dir_mn =   1;
+int time_hour   =   0;
+int time_minute =   0;
+int time_second =   0;
 
-bool first_loop = 1;
+// Time Keeping (NTP)
+const long utcOffsetInSeconds = -18000;
+char dasOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "north-america.pool.ntp.org", utcOffsetInSeconds);
 
 CRGB leds_hr[NUM_LEDS_HR];
 CRGB leds_mn[NUM_LEDS_MN];
@@ -33,6 +49,14 @@ CRGBPalette16 MyRainbow_pal = MyRainbow;
 
 void setup() {
   Serial.begin(115200);
+
+  // WiFiManager
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("7Seg_Clock");
+  Serial.println("connected... yay!");
+
+  timeClient.begin();
+  
   Serial.println("Starting Animation");
   FastLED.addLeds<WS2812B, DATA_PIN_HR, RGB>(leds_hr, NUM_LEDS_HR);
   FastLED.addLeds<WS2812B, DATA_PIN_MN, RGB>(leds_mn, NUM_LEDS_MN);
@@ -54,16 +78,20 @@ void loop() {
   }
   grad_mn = grad_mn + grad_dir_mn;
 
-  time_hour = time_hour + 1;
-  if (time_hour == 100) {
-    time_hour = 0;
+  timeClient.update();
+  time_hour   = timeClient.getHours();
+  time_minute = timeClient.getMinutes();
+  time_second = timeClient.getSeconds();
+  Serial.print("The current time is: "); Serial.print(time_hour); Serial.print(":"); Serial.print(time_minute); Serial.print(":"); Serial.println(time_second);
+  if (HOUR_FORMAT == 12 && time_hour > 12){
+    time_hour = time_hour - 12;
   }
   
-  Serial.print("setting new color (grad_hr, grad_mn): ("); Serial.print(grad_hr); Serial.print(","); Serial.print(grad_mn); Serial.println(")");
+  //Serial.print("setting new color (grad_hr, grad_mn): ("); Serial.print(grad_hr); Serial.print(","); Serial.print(grad_mn); Serial.println(")");
   // set LEDS in the set to the new color
   set_hr(time_hour);
-  set_mn(time_hour);
+  set_mn(time_minute);
   FastLED.show();
 
-  delay(250);
+  delay(1000);
 }
