e6ea955dd2da53875b5a6226368732c582265185
N Oliver <github@pixnbits.org>
2019-08-24 11:36:21 -0700

    fix(Segments): remove internal helper methods from headers

--- Software/src/Segments_Hour.h
+++ Software/src/Segments_Hour.h
@@ -3,14 +3,3 @@
 #include "Common.h"
 
 void set_hr(int hour_number);
-void set_hr_off(int array_start);
-void set_hr_0(int array_start);
-void set_hr_1(int array_start);
-void set_hr_2(int array_start);
-void set_hr_3(int array_start);
-void set_hr_4(int array_start);
-void set_hr_5(int array_start);
-void set_hr_6(int array_start);
-void set_hr_7(int array_start);
-void set_hr_8(int array_start);
-void set_hr_9(int array_start);
--- Software/src/Segments_Minute.h
+++ Software/src/Segments_Minute.h
@@ -3,13 +3,3 @@
 #include "Common.h"
 
 void set_mn(int min_number);
-void set_mn_0(int array_start);
-void set_mn_1(int array_start);
-void set_mn_2(int array_start);
-void set_mn_3(int array_start);
-void set_mn_4(int array_start);
-void set_mn_5(int array_start);
-void set_mn_6(int array_start);
-void set_mn_7(int array_start);
-void set_mn_8(int array_start);
-void set_mn_9(int array_start);
